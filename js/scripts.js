7f57717fc4e475208efa26e024f749217bbf9cc5
Nathan Schrader <schradermade@Nathans-MacBook-Pro.local>
2020-11-14 16:48:23 -0800

    add all variables

--- js/scripts.js
+++ js/scripts.js
@@ -7,19 +7,35 @@ function Pizza( guestName, toppings, size)  {
 }
 
 Pizza.prototype.calcPrice = function()  {
-
-}
+  let cost = 0;
+  if ( this.size === 'medium')  {
+    cost += 10;
+  }
+  if ( this.size === "large") {
+    cost += 15;
+  }
+  alert(cost);
+};
 
 $(document).ready(function() {
   $("#formOne").submit(function(event)  {
     event.preventDefault();
 
+    let pizzaOrder = new Pizza(guestName, toppings, size);
+    let pizzaPrice = pizzaOrder.calcPrice();
+    alert(pizzaPrice);
     let guestName = $("#guest-name").val();
     alert(guestName);
-    let toppings;
     let cheese = $("#cheese").val();
     alert(cheese);
     let pepperoni = $("#pepperoni").val();
     alert(pepperoni);
+    let toppings = [pepperoni, cheese];
+    alert(toppings);
+    let size = $("#size").val();
+    alert(size);
+    
   })
+
+  $("#results").empty().append(guestName + ", your" + size + "pizza will be $" + pizzaPrice );
 });
